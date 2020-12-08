6266f6a8e4c94ebde35237f544fe552e1f85a804
Romo_JJE_com1k05 <emanuelromo1234@gmail.com>
2020-12-08 00:13:14 -0200

    MODULO ADMINISTRACION modificado validacion usuario

--- MOD-Administracion.cpp
+++ MOD-Administracion.cpp
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 #include<ctype.h>
 #include<windows.h>
+#include<locale.h>
 
 struct usuarios{
 	char usuario[10];
@@ -15,6 +16,7 @@ void opcionesadm(int op,FILE *&arc);
 void registro_usuario_y_contra(FILE *&arc);
 
 main(){
+	setlocale(LC_CTYPE, "spanish");
 	char claveadministracion[]="VETeriAR012";
 	char intentoclave[30];
 	int intentos=4,opc=0;
@@ -24,7 +26,7 @@ main(){
 	do{								//en este do while valido que la persona que entre al area de administracion
 			if(intentos<4)			//tenga permiso para hacerlo
 			printf("Tiene %d intentos\n\n",intentos);
-		printf("Ingrese clave de administracion: ");
+		printf("Ingrese clave de administración: ");
 		_flushall();
 		gets(intentoclave);
 		intentos--;
@@ -47,14 +49,14 @@ void menuadm(int &op){
 	do{
 		op=0;
 		_flushall();
-	printf("\n\n\t\t\t   MODULO ADMINISTRACION\n");
+	printf("\n\n\t\t\t   MÓDULO ADMINISTRACIÓN\n");
 	printf("\t\t\t===========================\n\n");
 	printf("\t\t\t1.- Registrar veterinario\n\n");
 	printf("\t\t\t2.- Registrar asistente\n\n");
-	printf("\t\t\t3.- Atencion por veterinarios\n\n");
-	printf("\t\t\t4.- Ranking de Veterinarios por atencion\n\n");
-	printf("\t\t\t5.- Cerrar modulo\n\n");
-	printf("\t\t\tIngrese una opcion: ");
+	printf("\t\t\t3.- Atención por veterinarios\n\n");
+	printf("\t\t\t4.- Ranking de Veterinarios por atención\n\n");
+	printf("\t\t\t5.- Cerrar módulo\n\n");
+	printf("\t\t\tIngrese una opción: ");
 	scanf("%d",&op);
 	system("cls");
 	}while(op!=1 && op!=2 && op!=3 && op!=4 && op!=5);
@@ -82,7 +84,7 @@ bool repetido(char user[10],FILE *&arc){
 		if(strcmp(user,users.usuario)==0){
 		printf("\n");
 		system("color 0c");
-		printf("X Usuario '%s' ya registrado ingrese uno nuevo\n",user);
+		printf("X El '%s' usuario no está disponible ingrese uno nuevo\n",user);
 		system("pause");
 		system("color 07");
 		fclose(arc);
@@ -95,24 +97,24 @@ bool repetido(char user[10],FILE *&arc){
 	return false;
 }
 void usuario(char user[10],FILE *&arc){
-	int condiciones,may,num;
+	int condiciones,may,num,coinc,otros;
+	char conjunto[]= "+-*/?¿¡!";
 	
 	do{
 	do{
 		system("cls");
-		condiciones=4;
-		may=0;
-		num=0;
+		condiciones=5,may=0,num=0,otros=0;
+		
 	printf("\nIngrese un nombre de usuario: ");
 	_flushall();
 	gets(user);
 	printf("\n");
-	if(strlen(user)<4 or strlen(user)>10){
-		printf("X El usuario ingresado debe tener entre cuatro y diez caracteres\n");
+	if(strlen(user)<6 or strlen(user)>10){
+		printf("X El usuario ingresado debe tener entre seis y diez caracteres\n");
 		condiciones--;
 	}
 	if(user[0]<'a' or user[0]>'z'){
-		printf("X La primera letra del usuario ingresado debe ser minuscula\n");
+		printf("X La primera letra del usuario ingresado debe ser minúscula\n");
 		condiciones--;
 	}
 	for(int i=0;i<strlen(user);i++){
@@ -120,22 +122,35 @@ void usuario(char user[10],FILE *&arc){
 			may++;
 		if(isdigit(user[i]))
 			num++;
+		if(!isdigit(user[i]) && !isalpha(user[i])){
+			coinc=0,otros=0;
+			for(int j=0;j<strlen(conjunto);j++){
+				if(user[i]==conjunto[j])
+				coinc++;
+				if(j==strlen(conjunto)-1 and coinc==0)
+				otros++;
+			}
+		}
 	}
 	if(may<2){
-		printf("X El usuario ingresado debe tener al menos dos letras mayusculas\n");
+		printf("X El usuario ingresado debe tener al menos dos letras mayúsculas\n");
 		condiciones--;
 	}
 	if(num>3){
-		printf("X El usuario puede tener como maximo tres numeros\n");
+		printf("X El usuario puede tener como máximo tres números\n");
 		condiciones--;
 	}
-	if(condiciones<4){
+	if(otros>0){
+		printf("X El usuario solo admite los simbolos del conjunto {+,-,*,/,?,¿,¡,!}\n");
+		condiciones--;
+	}
+	if(condiciones<5){
 		printf("\n");
 		system("color 0c");
 		system("pause");
 		system("color 07");
 		}
-	}while(condiciones<4);
+	}while(condiciones<5);
 	}while(repetido(user,arc));
 	
 }
@@ -144,13 +159,14 @@ void registro_usuario_y_contra(FILE *&arc){
 	
 	usuarios users;
 	
-	printf("\n\t\tCREACION DE USUARIO\n\nCondiciones de usuarios validos:\n\n");
-	printf(". No repetidos\n. Comenzar con minusculas\n. Tener al menos dos letras mayusculas\n");
-	printf(". Tener como maximo tres numeros\n. Entre cuatro y diez caracteres\n\n");
+	printf("\n\t\tCREACIÓN DE USUARIO\n\nCondiciones de usuarios validos:\n\n");
+	printf(". Que no esté en uso\n. Comenzar con minúsculas\n. Tener al menos dos letras mayúsculas\n");
+	printf(". Tener como máximo tres numeros\n. Entre seis y diez carácteres\n");
+	printf(". Admitirá los simbolos del conjunto: {+,-,*,/,¿,?,¡,!}\n\n");
 	system("pause");
 	
 	usuario(user,arc);
-	printf("Usuario valido, Ingrese una contrasena: ");
+	printf("Usuario válido, Ingrese una contraseña: ");
 	_flushall();
 	gets(pass);
 	printf("Y un nombre");
@@ -162,7 +178,7 @@ void registro_usuario_y_contra(FILE *&arc){
 	strcpy(users.contrasena,pass);
 	strcpy(users.Apellido_y_nombre,nombre);
 	fwrite(&users,sizeof(usuarios),1,arc);
-	printf("REGISTRADo");
+	printf("REGISTRADO");
 	fclose(arc);
 }
 /*
